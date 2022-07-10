765bbda946afa41a2f97843c5f5a0244d5a6c3ed
Marc Soda Jr <m@soda.fm>
2022-07-10 19:34:05 -0400

    new banner, better dispatcher, nearly done clap_def

---
+++ server/target/debug/deps/taskman_server-b1607579ee6a6320.d
@@ -0,0 +1,7 @@
+/home/marc/working/dev/rust/raskman/server/target/debug/deps/taskman_server-b1607579ee6a6320.rmeta: src/main.rs
+
+/home/marc/working/dev/rust/raskman/server/target/debug/deps/taskman_server-b1607579ee6a6320.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
